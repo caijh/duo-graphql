559c13cf07c081158b8ee88db83b4948d7f898a7
蔡俊辉 <caijunhui@pcitech.com>
2021-07-15 14:32:44 +0800

    Fix 拼写错误

--- graphql-register/src/main/java/com/github/caijh/graphql/register/server/GraphqlEngineService.java
+++ graphql-register/src/main/java/com/github/caijh/graphql/register/server/GraphqlEngineService.java
@@ -5,16 +5,16 @@ import java.util.List;
 /**
  * graphql provider registry service
  *
- * @param <T> grpahql provider info
+ * @param <T> graphql provider info
  * @author xuwenzhen
- * @date 2019/4/9
+ * @since 2019/4/9
  */
 public interface GraphqlEngineService<T> {
 
     /**
      * registry graphql provider to registry center, eg. zookeeper or redis
      *
-     * @param key
+     * @param key                     key for graphql provider
      * @param providerServiceDataList graphql provider info list
      */
     void registry(String key, List<T> providerServiceDataList);
